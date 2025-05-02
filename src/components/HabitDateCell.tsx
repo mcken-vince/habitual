cfff18f50a747adbf11004d4cfdd1ce6010d3005
mcken-vince <mcken.vince@gmail.com>
2025-05-02 11:09:44 -0600

    Slight refactor, switch x and check symbols

--- src/components/HabitDateCell.tsx
+++ src/components/HabitDateCell.tsx
@@ -8,29 +8,29 @@ interface HabitDateCellProps {
 }
 
 export const HabitDateCell = ({ habit, date, onClick }: HabitDateCellProps) => {
-  return (<div key={date} className="flex flex-col items-center">
-    <Button
-      onClick={onClick}
-      variant="ghost"
-      className={`w-12 h-12 flex items-center justify-center rounded-md bg-transparent hover:bg-inherit`}
+  const isCompleted = habit.history[date];
+  const textColorClass = isCompleted ? "text-foreground" : "text-gray-200";
 
-    >
-      <div className={`${habit.history[date] ? "" : "text-gray-200"} `}>
+  return (
+    <div key={date} className="flex flex-col items-center">
+      <span
+        onClick={onClick}
+        // variant="ghost"
+        className="w-12 h-12 flex items-center justify-center rounded-md bg-transparent hover:bg-inherit"
+      >
         {habit.type === "boolean" ? (
-          habit.history[date] ? (
-            "✔"
-          ) : (
-            "✖"
-          )
+          <span className={`text-2xl font-bold ${textColorClass}`}>
+            {isCompleted ? "✓" : "✗"}
+          </span>
         ) : (
-          <>
-            <span className="text-lg font-semibold flex flex-col">{habit?.history[date] || 0}</span>
+          <div className={`flex flex-col items-center ${textColorClass}`}>
+            <span className="text-lg font-semibold">{habit.history[date] || 0}</span>
             {habit.type === "measurable" && (
               <span className="text-xs mt-1">{habit.unit}</span>
             )}
-          </>
+          </div>
         )}
-      </div>
-    </Button>
-  </div>)
-}
+      </span>
+    </div>
+  );
+};
