91147148f7411e641742ac53ab9e3dfc1c30d5b6
mcken-vince <mcken.vince@gmail.com>
2025-05-13 14:11:20 -0600

    Fix getDatesInRange logic

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -3,7 +3,8 @@ export function getDatesInRange(start: Date, end: Date): string[] {
   const endDate = new Date(end);
   const length = Math.round((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
   return Array.from({ length }, (_, i) => {
-    startDate.setDate(startDate.getDate() - i);
-    return startDate.toISOString().split("T")[0];
+    const date = new Date();
+    date.setDate(startDate.getDate() - i);
+    return date.toISOString().split("T")[0];
   }).reverse();
 }
