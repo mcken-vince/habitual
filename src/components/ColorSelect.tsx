c152009229bc4e280cf368be5f95fba3c25684fa
mcken-vince <mcken.vince@gmail.com>
2025-06-05 14:25:21 -0600

    Remove inaccessible colors from ColorSelect

--- src/components/ColorSelect.tsx
+++ src/components/ColorSelect.tsx
@@ -4,13 +4,11 @@ const colors = [
   { value: "#FF0000", label: "Red" },
   { value: "#00FF00", label: "Green" },
   { value: "#0000FF", label: "Blue" },
-  { value: "#FFFF00", label: "Yellow" },
   { value: "#FFA500", label: "Orange" },
   { value: "#800080", label: "Purple" },
   { value: "#00FFFF", label: "Cyan" },
   { value: "#FFC0CB", label: "Pink" },
   { value: "#808080", label: "Gray" },
-  { value: "#000000", label: "Black" },
 ]
 
 const ColorSelectItem = ({ value, label }: { value: string; label: string }) => {
