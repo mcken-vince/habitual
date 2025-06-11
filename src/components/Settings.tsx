a059d24d23fce9534c3db9b68d5212a53643c8ae
mcken-vince <mcken.vince@gmail.com>
2025-06-11 14:35:38 -0600

    Add back button to Settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,10 +1,15 @@
+import { ArrowLeftIcon } from "lucide-react";
+import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 
 export const Settings = ({ open, onClose }: { open: boolean, onClose: (value: boolean) => void }) => {
   return (
     <Sheet open={open} onOpenChange={onClose}>
       <SheetContent side="bottom" className="h-full p-4" hideCloseButton>
-        <SheetHeader>
+        <SheetHeader className="flex flex-row items-center gap-4">
+          <Button variant="ghost" size="icon" onClick={() => onClose(false)} className="p-2">
+            <ArrowLeftIcon className="w-5 h-5" />
+          </Button>
           <SheetTitle>Settings</SheetTitle>
         </SheetHeader>
         {/* Add your settings form or content here */}
