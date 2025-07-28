fd70b428ef159af9ca2ec1cbc63d929c0a56342f
mcken-vince <mcken.vince@gmail.com>
2025-07-28 10:20:04 -0600

    Update Jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,21 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        baseUrl: '.',
+        paths: {
+          '@/*': ['./src/*']
+        }
+      }
+    }],
   },
   testRegex: '(/__tests__/.*|(\\.|/))(test|spec)\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  rootDir: '.',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
 };
