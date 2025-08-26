dd3c8bd8579f475e600c6130babc20d79aebeb4b
mcken-vince <mcken.vince@gmail.com>
2025-08-26 10:53:54 -0600

    Update test configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        baseUrl: '.',
-        paths: {
-          '@/*': ['./src/*']
-        }
-      }
+      tsconfig: 'tsconfig.test.json'
     }],
   },
   testRegex: '(/__tests__/.*|(\\.|/))(test|spec)\\.(jsx?|tsx?)$',
@@ -19,4 +14,22 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   rootDir: '.',
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts',
+    '!src/setupTests.ts',
+    '!src/components/ui/*.tsx', // Exclude shadcn/ui components from coverage
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageDirectory: 'coverage',
 };
